099657e69ba31787baeb835d2babda8d3f9fdd62
Jesus Andres Hincapie <jahlon@gmail.com>
2018-10-24 10:54:13 -0500

    implemetó la funcionalidad de las estrellas

--- Espacio.java
+++ Espacio.java
@@ -11,7 +11,7 @@ public class Espacio extends World {
     private Boton botonRetry;
     
     public Espacio() {    
-        super(800, 600, 1); 
+        super(800, 600, 1, false); 
         
         // Configurar fondo negro
         GreenfootImage fondo = getBackground();
@@ -20,5 +20,16 @@ public class Espacio extends World {
         
         // Asignar el orden en que se pintan los objetos
         setPaintOrder(Boton.class, Counter.class, Nave.class, Asteroide.class, Estrella.class);
+    }
+    
+    private void generarEstrella() {
+        Estrella estrella = new Estrella();
+        int x = Greenfoot.getRandomNumber(getWidth());
+        int y = -5;
+        addObject(estrella, x, y);
+    }
+    
+    public void act() {
+        generarEstrella();
     }
 }
