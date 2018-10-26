48962f8dd7b5b5742472e8075d0f5e5c6a667f6f
jahlon <jahlon@gmail.com>
2018-10-26 11:04:58 -0500

    se modificó Espacio.java para agregar la funciolidad de inicio del juego

--- Espacio.java
+++ Espacio.java
@@ -5,6 +5,7 @@ public class Espacio extends World {
     private GreenfootSound sonidoDeFondo;
     private int tiempoJuego;
     private int tiempoGeneracionAsteroides;
+    private SimpleTimer timerJuego;
     
     private Nave nave;
     private Counter contador;
@@ -18,8 +19,40 @@ public class Espacio extends World {
         fondo.setColor(Color.BLACK);
         fondo.fill();
         
+        // inicializar timer
+        timerJuego = new SimpleTimer();
+        
+        // inicializar sonido de fondo
+        sonidoDeFondo = new GreenfootSound("cruising.mp3");
+        
+        contador = new Counter("Tiempo: ");
+        addObject(contador, 70, 20);
+        
+        inicializarNave();
+        
         // Asignar el orden en que se pintan los objetos
         setPaintOrder(Boton.class, Counter.class, Nave.class, Asteroide.class, Estrella.class);
+    }
+    
+    private void inicializarNave() {
+        nave = new Nave();
+        int x = getWidth()/2;
+        int y = getHeight() - nave.getImage().getHeight()/2;
+        addObject(nave, x, y);
+    }
+    
+    public void reproducirSonido() {
+        if( !sonidoDeFondo.isPlaying() ) {
+           sonidoDeFondo.playLoop();
+        }
+    }
+    
+    private void contarTiempo() {
+        if( timerJuego.millisElapsed() >= 1000 ) {
+            timerJuego.mark();
+            tiempoJuego++;
+            contador.setValue(tiempoJuego);
+        }
     }
     
     private void generarEstrella() {
@@ -30,6 +63,8 @@ public class Espacio extends World {
     }
     
     public void act() {
+        contarTiempo();
         generarEstrella();
+        reproducirSonido();
     }
 }
